e8e2c4d7aab437a2aa8df154bda1d0b9838f479d
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-05-12 13:30:44 -0700

    Build useEffect with data fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+import { BASE_URL, API_KEY } from './Constants';
 
 function App() {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/planetary/apod?api_key=${API_KEY}`)
+        .then(function (response) {
+            console.log(response.data);
+            setData(response.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    }, [])
+
   return (
     <div className="App">
       <p>
