43ace35d907461d989ade1154f8032222acf0403
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-05-13 13:45:41 -0700

    Add style-components to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import styled from 'styled-components'
 
-const Header = (props) => {
-    const { data } = props;
+const StyledHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 2%;
+  box-shadow: 0px 10px 30px #1f1f1f;
 
+  background-color: ${props => props.theme.primaryColor};
+  color: ${props => props.theme.textColor};
+
+  h1 {
+    font-size: calc(40px + 2vmin);
+    margin: 0;
+    padding-bottom: 2%;
+  }
+
+  h2 {
+    font-size: calc(16px + 2vmin);
+    font-style: italic;
+  }
+`
+
+export default function Header({ data }) {
     return (
-        <div className='header-container'>
+        <StyledHeader>
             <h1>{data.title}</h1>
+            <h2>NASA Photo of the Day</h2>
             <h2>{data.date}</h2>
-        </div>
-    );
-};
-
-export default Header;
+        </StyledHeader>
+    )
+}
