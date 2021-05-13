6f910f69647bf78763baa8814a2882577074975f
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-05-13 14:12:04 -0700

    Add style-components to description

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
 import Header from './components/Header'
 import Photo from './components/Photo'
 import Description from './components/Description'
--- src/components/Description.js
+++ src/components/Description.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React from 'react'
+import styled from 'styled-components'
 
-const Description = (props) => {
-    const { data } = props;
+const StyledDescription = styled.div`
+    margin: 0 4%;
+    margin-bottom: 10%;
+    background-color: #272640;
+    padding: 1% 2%;
+    font-size: 20px;
+    color: rgb(218, 218, 218);
+    line-height: 1.5;
+    font-style: italic;
+    font-weight: 500;
+    box-shadow: 0px 0px 30px 5px #1f1f1f;
+`
 
+export default function Description({ data }) {
     return (
-        <div className='description-container'>
+        <StyledDescription>
             <p>{data.explanation}</p>
-        </div>
+        </StyledDescription>
     );
 };
-
-export default Description;
